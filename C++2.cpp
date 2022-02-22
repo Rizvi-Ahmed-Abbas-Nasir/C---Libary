74186c1fc2d693b6ea0758803bbcb635f8001bcf
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2022-02-22 16:47:28 +0530

    C++ Libary

--- C++2.cpp
+++ C++2.cpp
@@ -6,12 +6,19 @@ int main()
     int First {5}; // Curle Brace initialization 
     int Second = 5; // Assignment initialization
     int Third (5); // Parenthesis initialization
-    auto automatic = 100;
+    auto automatic = 100; // It's Variable Declare according to the value  
+    short int Short_int  (5); //Shorten the based store value of integer like 4 to 2
+    long int Long_int (7); // increase the based store value of integer like 4 to 8
+    signed int Singed_int {-5}; // Only for negative integer
+    unsigned int Unsigend_int {7};  // only for positive integer
+
 
     cout<<"First: "<<First<<endl;
     cout<<"Second: "<<Second<<endl;
-    cout<<"Third: "<<Third<<endl;
-    cout<<"Auto: " <<automatic<<endl;
+    cout<<"Third: "<<Short_int<<endl;
+    cout<<"Auto: " <<Singed_int<<endl;
+
+    cout<<"The Size Of INTEGERS: "<<sizeof(Third)<<endl;
 
 return 0;
 }
